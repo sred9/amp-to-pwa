d6fd44b75bfdc224229479142a9ae188c3396472
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-05-13 11:29:45 +0530

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,15 +37,7 @@ exports.createPages = ({ graphql, actions }) => {
       const previous = index === posts.length - 1 ? null : posts[index + 1].node
       const next = index === 0 ? null : posts[index - 1].node
 
-      createPage({
-        path: `posts${post.node.fields.slug}`,
-        component: blogPost,
-        context: {
-          slug: post.node.fields.slug,
-          previous,
-          next,
-        },
-      })
+  
     })
   })
 }
